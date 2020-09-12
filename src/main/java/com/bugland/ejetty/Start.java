6aea54c03ccbbec46c1af146e91536f23d914410
Erich Bremer <erich@ebremer.com>
2020-09-12 10:20:01 -0400

    wip

--- src/main/java/com/bugland/ejetty/Start.java
+++ src/main/java/com/bugland/ejetty/Start.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.ejetty;
+package com.bugland.ejetty;
 
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.HttpConfiguration;
